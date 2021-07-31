ed00b87ff112bc7d88d85509b3980dc0f7116235
Justin Turner <jmturner638@gmail.com>
2021-07-31 00:13:58 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    pass
+
+def sum(number1, number2, number3, number4):
+    return number1 + number2 + number3 + number4
 
 if __name__ == '__main__':
     main()
